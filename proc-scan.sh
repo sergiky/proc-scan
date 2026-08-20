966e5f06fae796ffa7fd03dd65ff795c5309a7eb
Sergio Guijarro <90042979+sergiky@users.noreply.github.com>
2026-08-20 16:15:34 +0200

    Update proc-scan.sh

--- proc-scan.sh
+++ proc-scan.sh
@@ -5,7 +5,7 @@ new_proc=
 
 while true; do
 	new_proc=$(ps -eo pid,ruser,command | tail -n 6 | grep -vE "tail|grep|ps|bash|kworker")
-	if [[ "$new_proc" == "$proc" ]]; then
+	if [[ "$new_proc" == "$proc" || "$new_proc" == "" ]]; then
 		:
 	else
 		echo "$new_proc"
